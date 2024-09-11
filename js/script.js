6411d8782203da4ae9b3d202299c918901705beb
kauanschavaroski <162127624+kauanschavaroski@users.noreply.github.com>
2024-09-11 10:19:43 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import {aleatorio} from './aletorio.js';
+import {aleatorio, nome} from './aletorio.js';
 import {perguntas} from './perguntas.js';
 
 const caixaPrincipal = document.querySelector(".caixa-principal");
@@ -41,7 +41,7 @@ function respostaSelecionada(opcaoSelecionada){
 }
 
 function mostraResultado(){
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = `Em 2049, $(nome)`;
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
     caixaResultado.classlist.add("mostrar");
